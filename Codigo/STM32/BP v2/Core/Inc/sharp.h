2ae15de354f9f0617db606122dcb1631b96e607a
maicumaicu <71650151+maicumaicu@users.noreply.github.com>
2022-11-17 19:06:39 -0300

    df

---
+++ Codigo/STM32/BP v2/Core/Inc/sharp.h
@@ -0,0 +1,18 @@
+/*
+ * sharp.h
+ *
+ *  Created on: Aug 21, 2022
+ *      Author: usuario
+ */
+
+#ifndef INC_SHARP_H_
+#define INC_SHARP_H_
+#include "stm32f1xx_hal.h"
+#include "main.h"
+
+uint32_t* readSensor(uint32_t *value);
+float lecturaSensor(int direccion, float *values);
+float lecSensor(int n, uint32_t *values);
+int lecturaCNY70(int n, uint32_t values[10]);
+
+#endif /* INC_SHARP_H_ */
